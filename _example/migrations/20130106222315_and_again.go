bbaba27f404c5d95f32c928ab2cbe0c6c512ded3
Cody Nguyen <cody.haind@gmail.com>
2016-07-23 11:55:39 +0700

    pass db connection to go migration script

--- _example/migrations/20130106222315_and_again.go
+++ _example/migrations/20130106222315_and_again.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 )
 
-func Up_20130106222315(txn *sql.Tx) {
+func Up_20130106222315(txn *sql.Tx, db *sql.DB) {
 	fmt.Println("Hello from migration 20130106222315 Up!")
 }
 
-func Down_20130106222315(txn *sql.Tx) {
+func Down_20130106222315(txn *sql.Tx, db *sql.DB) {
 	fmt.Println("Hello from migration 20130106222315 Down!")
 }
